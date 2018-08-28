40fa6db778875792b339526d2792992656048209
Ignacio R Espino <irespino@capozios.com>
2018-08-28 00:41:42 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+badge_maker(name)
+return "Hello, my name is #{name}."
+end
+
+batch_badge_creator(array)
+  batch_array = []
+  array.each { |name|
+  batch_array.push(badge_maker(name))
+  }
+  return batch_array
+end
+
+assign_room(array)
+room_asssignments = []
+array.each_with_index { |name, index|
+  room_asssignments.push("Hello, #{name}! You'll be assigned to room #{index + 1}")
+}
+return room_asssignments
+end
